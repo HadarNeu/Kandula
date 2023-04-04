05447be1330f93f2c98f4a25820341d7ad7eaeb5
HadarNeu <hadarnoylander@gmail.com>
2023-04-04 15:03:54 +0000

    end of previous session

---
+++ terraform/vpc-module/main.tf
@@ -0,0 +1,121 @@
+#### Getting the data of the available availability zones ####
+data "aws_availability_zones" "available" {}
+
+#### Creating a VPC   ######
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr_block
+
+  tags = {
+    "Name" = "${var.vpc_name}"
+  }
+}
+
+#### Creating public subnets #####
+resource "aws_subnet" "public" {
+  map_public_ip_on_launch = "true"
+  count                   = length(var.public_subnets_cidr_list)
+  cidr_block              = var.public_subnets_cidr_list[count.index]
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    "Name" = "public-subnet-${regex(".$", data.aws_availability_zones.available.names[count.index])}-${var.vpc_name}"
+  }
+}
+
+#### Creating private subnets #####
+resource "aws_subnet" "private" {
+  count                   = length(var.private_subnets_cidr_list)
+  cidr_block              = var.private_subnets_cidr_list[count.index]
+  vpc_id                  = aws_vpc.vpc.id
+  map_public_ip_on_launch = "false"
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    "Name" = "private-subnet-${regex(".$", data.aws_availability_zones.available.names[count.index])}-${var.vpc_name}"
+  }
+}
+
+#### Creating Internet gateway ####
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    "Name" = "igw-${var.vpc_name}"
+  }
+}
+
+#### Elasic IPs ####
+resource "aws_eip" "eip" {
+  count = length(var.private_subnets_cidr_list)
+
+  tags = {
+    "Name" = "eip-nat-${regex(".$", data.aws_availability_zones.available.names[count.index])}-${var.vpc_name}"
+  }
+}
+
+#### NAT Gateways ####
+resource "aws_nat_gateway" "nat" {
+  count         = length(var.private_subnets_cidr_list)
+  allocation_id = aws_eip.eip.*.id[count.index]
+  subnet_id     = aws_subnet.public.*.id[count.index]
+
+  tags = {
+    "Name" = "nat-${regex(".$", data.aws_availability_zones.available.names[count.index])}-${var.vpc_name}"
+  }
+}
+
+
+#####################
+###### ROUTING ######
+#####################
+
+#### Creating a public route table ####
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    "Name" = "public-route-table-${regex(".$", data.aws_availability_zones.available.names[count.index])}-${var.vpc_name}"
+  }
+}
+
+#### Creating private route tables ####
+resource "aws_route_table" "private_route_tables" {
+  count  = length(var.private_subnets_cidr_list)
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    "Name" = "private-route-table-${regex(".$", data.aws_availability_zones.available.names[count.index])}-${var.vpc_name}"
+  }
+}
+
+#### Public route table association ####
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets_cidr_list)
+  subnet_id      = aws_subnet.public.*.id[count.index]
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+#### Private route table association ####
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets_cidr_list)
+  subnet_id      = aws_subnet.private.*.id[count.index]
+  route_table_id = aws_route_table.private_route_tables[count.index].id
+}
+
+#### Public route for the public route table ####
+resource "aws_route" "public" {
+  count          = length(var.public_subnets_cidr_list)
+  route_table_id         = aws_route_table.route_tables[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+#### Public route for private route tables ####
+resource "aws_route" "private" {
+  count                  = length(var.private_subnets_cidr_list)
+  route_table_id         = aws_route_table.route_tables.*.id[count.index + 1]
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.*.id[count.index]
+}
+
---
+++ terraform/vpc-module/variables.tf
@@ -0,0 +1,34 @@
+variable "vpc_cidr_block" {
+  type = string
+  description = "The cidr block of the VPC, for ex- '10.0.0.0/16'"
+}
+
+variable "private_subnets_cidr_list" {
+  type    = list(string)
+  description = "List of private subnets of the VOC for ex-['10.0.2.0/24', '10.0.3.0/24']"
+}
+
+variable "public_subnets_cidr_list" {
+  type    = list(string)
+  description = "List of public subnets of the VPC for ex-['10.0.5.0/24', '10.0.6.0/24']"
+}
+
+variable "vpc_name" {
+  type    = list(string)
+  description = "The name of the VPC"
+  default = "Kandula-VPC"
+}
+
+
+# variable "public_route_tables_name_list" {
+#   type    = list(string)
+#   description = "List of the names of the route-tables (Module creates two RTBS, one public [0] and one private [1]"
+#   default = ["public", "private-a", "private-b"]
+# }
+
+# variable "private_route_tables_name_list" {
+#   type    = list(string)
+#   description = "List of the names of the route-tables (Module creates two RTBS, one public [0] and one private [1]"
+#   default = ["public", "private-a", "private-b"]
+# }
+
