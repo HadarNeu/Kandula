eb75fc831ed9847605dcf06281752dc9448d51c6
HadarNeu <hadarnoylander@gmail.com>
2023-04-06 12:18:56 +0000

    playing w data source ubuntu ami

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,11 +1,11 @@
 data "aws_availability_zones" "available" {}
 
-data "aws_ami" "ubuntu-18" {
+data "aws_ami" "ubuntu-ami" {
   most_recent = true
   owners      = [var.ubuntu_account_number]
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-${var.ubuntu_version}-amd64-server-*"]
   }
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,70 @@
+variable "aws_region" {
+  default = "us-west-2"
+  type    = string
+}
+
+variable "instance_type" {
+  description = "The type of the ec2"
+  default     = "t2.micro"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The key name of the Key Pair to use for the instance"
+  default     = "coredns_key"
+  type        = string
+}
+
+variable "ubuntu_account_number" {
+  description = "The AWS account number of the offical Ubuntu Images"
+  default     = "099720109477"
+  type        = string
+}
+
+variable "ubuntu_version" {
+  description = "The version of the offical Ubuntu Images"
+  default     = "22.04"
+  type        = string
+}
+
+variable "nginx_instances_count" {
+  description = "The number of Nginx instances to create"
+  default     = 2
+}
+
+variable "DB_instances_count" {
+  description = "The number of DB instances to create"
+  default     = 2
+}
+
+variable "nginx_root_disk_size" {
+  description = "The size of the root disk"
+  default     = 10
+}
+
+variable "nginx_encrypted_disk_size" {
+  description = "The size of the secondary encrypted disk"
+  default     = 10
+}
+
+variable "nginx_encrypted_disk_device_name" {
+  description = "The name of the device of secondary encrypted disk"
+  default     = "xvdh"
+  type        = string
+}
+
+variable "volumes_type" {
+  description = "The type of all the disk instances in my project"
+  default     = "gp2"
+}
+
+variable "owner_tag" {
+  description = "The owner tag will be applied to every resource in the project through the 'default variables' feature"
+  default     = "Ops-School"
+  type        = string
+}
+
+variable "purpose_tag" {
+  default = "Whiskey"
+  type    = string
+}
