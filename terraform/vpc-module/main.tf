700a8f498540666d25c4f26380410764edcc6341
HadarNeu <hadarnoylander@gmail.com>
2023-04-08 11:43:25 +0000

    accindental char

--- terraform/vpc-module/main.tf
+++ terraform/vpc-module/main.tf
@@ -12,8 +12,7 @@ resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr_block
 
   tags = {
-    "Name" = "${var.vpc_name}",
-c
+    "Name" = "${var.vpc_name}"
   }
 
 #   public_subnet_tags = {
