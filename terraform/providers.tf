4e45080a7048de3b5392e5c150b0817add031090
HadarNeu <hadarnoylander@gmail.com>
2023-04-08 09:11:06 +0000

    had problems w init- solved

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,13 +1,34 @@
-provider "aws" {
-  region = var.aws_region
+# provider "aws" {
+#   region = var.aws_region
+# }
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.30.0"
+#     }
+#   }
+#   required_version = "~> 1.0"
+# }
+
+# # Kubernetes provider
+# # https://learn.hashicorp.com/terraform/kubernetes/provision-eks-cluster#optional-configure-terraform-kubernetes-provider
+# # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/terraform/kubernetes/deploy-nginx-kubernetes
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.30.0"
+resource "kubernetes_service_account" "opsschool_sa" {
+  metadata {
+    name      = local.k8s_service_account_name
+    namespace = local.k8s_service_account_namespace
+    annotations = {
+      "eks.amazonaws.com/role-arn" = module.iam_iam-assumable-role-with-oidc.iam_role_arn
     }
   }
-  required_version = "~> 1.0"
+  depends_on = [module.eks]
 }
---
+++ terraform/versions.tf
@@ -0,0 +1,34 @@
+
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = ">=2.18.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.30.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.1"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.2.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
